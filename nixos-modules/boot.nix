9b0eac4e5e81d3f9730dbf8f3e5b7ccf572604f1
Moritz Vogel <moritzv7@gmail.com>
2023-07-07 15:06:52 +0200

    Enable cfs-zen-tweaks and disable NMI watchdog

--- nixos-modules/boot.nix
+++ nixos-modules/boot.nix
@@ -3,6 +3,8 @@
 {
   boot.kernelPackages = pkgs.linuxKernel.packages.linux_6_4;
 
+  programs.cfs-zen-tweaks.enable = true;
+
   boot.kernel.sysctl = {
     # Make system more responsive in case of OOM
     "vm.swappiness" = 10;
@@ -12,6 +14,8 @@
     "net.core.wmem_default" = 21299200;
     "net.core.wmem_max" = 21299200;
   };
+
+  boot.kernelParams = [ "nowatchdog" ];
 
   boot.tmp.useTmpfs = true;
 }
