61e97d269f79b7c7da442c9eafe62464882041cc
Moritz Vogel <moritzv7@gmail.com>
2023-07-08 23:18:11 +0200

    Install RPCS3 and PCSX2

--- nixos-modules/gaming.nix
+++ nixos-modules/gaming.nix
@@ -7,11 +7,29 @@
     dedicatedServer.openFirewall = true;
   };
 
+  # for RPCS3
+  security.pam.loginLimits = [
+    {
+      domain = "moritz";
+      item = "memlock";
+      type = "soft";
+      value = 2097152;
+    }
+    {
+      domain = "moritz";
+      item = "memlock";
+      type = "hard";
+      value = 2097152;
+    }
+  ];
+
   environment.systemPackages = with pkgs; [
     heroic
     prismlauncher
     yuzu-mainline
     dolphin-emu
+    pcsx2
+    rpcs3
     mangohud
   ];
 
