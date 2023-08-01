c61992fcd38f9aa1b8d433a6c53202bd20d8cabf
Moritz Vogel <moritzv7@gmail.com>
2023-08-01 22:12:55 +0200

    Move soundblaster workaround to separate file

--- hosts/rotkehlchen/configuration.nix
+++ hosts/rotkehlchen/configuration.nix
@@ -3,6 +3,7 @@
 {
   imports = [ # Include the results of the hardware scan.
     ./hardware-configuration.nix
+    ./soundblaster.nix
     ../../nixos-modules/boot.nix
     ../../nixos-modules/nix.nix
     ../../nixos-modules/console.nix
@@ -61,59 +62,6 @@
 
   # Needed for ddcutil
   hardware.i2c.enable = true;
-
-  # Workaround for Creative Sound BlasterX G6
-  systemd = let
-    # Wait until wireplumber has set the alsa options. This takes a while especially after suspend.
-    amixerWaitScript = ''
-      ${pkgs.coreutils-full}/bin/sleep 10
-    '';
-
-    alsaOptionsScript = ''
-      ${pkgs.alsaUtils}/bin/amixer -D hw:G6 sset 'PCM Capture Source' 'External Mic'
-      ${pkgs.alsaUtils}/bin/amixer -D hw:G6 sset 'External Mic',0 Capture cap
-      ${pkgs.alsaUtils}/bin/amixer -D hw:G6 sset 'External Mic',0 Capture 9dB
-      ${pkgs.alsaUtils}/bin/amixer -D hw:G6 sset 'Line In',0 Capture nocap
-      ${pkgs.alsaUtils}/bin/amixer -D hw:G6 sset 'S/PDIF In',0 Capture nocap
-      ${pkgs.alsaUtils}/bin/amixer -D hw:G6 sset 'What U Hear',0 Capture nocap
-    '';
-  in {
-    services.soundblaster-reset-usb = {
-      script =
-        "${pkgs.usb-modeswitch}/bin/usb_modeswitch -v 0x041e -p 0x3256 --reset-usb";
-      wantedBy = [ "multi-user.target" ];
-      serviceConfig = { Type = "oneshot"; };
-    };
-
-    user.services.soundblaster-set-alsa-options = {
-      script = alsaOptionsScript;
-      preStart = amixerWaitScript;
-      wantedBy = [ "pipewire.service" ];
-      after = [ "pipewire.service" "wireplumber.service" ];
-      serviceConfig = { Type = "oneshot"; };
-    };
-
-    services.soundblaster-set-alsa-options = {
-      script = alsaOptionsScript;
-      preStart = amixerWaitScript;
-      wantedBy = [
-        "suspend.target"
-        "hibernate.target"
-        "hybrid-sleep.target"
-        "suspend-then-hibernate.target"
-      ];
-      serviceConfig = {
-        Type = "oneshot";
-        User = "moritz";
-        Group = "users";
-      };
-    };
-  };
-
-  boot.extraModprobeConfig = ''
-    options snd_usb_audio id=G6 index=0
-    options snd_hda_intel id=HDMI,Generic index=1,2 enable=0,0
-  '';
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
