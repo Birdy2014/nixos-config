af2ef32d632b4705cde2fdc674f380ae12a2b906
Moritz Vogel <moritzv7@gmail.com>
2023-08-07 22:59:00 +0200

    Run nixfmt on git.nix

--- home-modules/git.nix
+++ home-modules/git.nix
@@ -7,12 +7,8 @@
     userEmail = "moritzv7@gmail.com";
 
     extraConfig = {
-      core = {
-        autocrlf = "input";
-      };
-      pull = {
-        ff = "only";
-      };
+      core = { autocrlf = "input"; };
+      pull = { ff = "only"; };
     };
 
     difftastic = {
