c4b1e04217297d7587c103f694045ece72a90da0
Moritz Vogel <moritzv7@gmail.com>
2023-08-07 21:21:35 +0200

    Encrypt all the drives

--- hosts/rotkehlchen/hardware-configuration.nix
+++ hosts/rotkehlchen/hardware-configuration.nix
@@ -9,14 +9,19 @@
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
 
+  # Samsung SSD 870 EVO 4TB
+
+  boot.initrd.luks.devices."nixos-root" = {
+    device = "/dev/disk/by-uuid/ec1f1df4-349f-43ca-aa22-8c60a5845a5a";
+    bypassWorkqueues = true;
+    allowDiscards = true;
+  };
+
   fileSystems."/" = {
     device = "/dev/disk/by-uuid/f313ecbb-7133-4a4e-bf77-272570e8a286";
     fsType = "btrfs";
     options = [ "subvol=root" "compress=zstd:1" ];
   };
-
-  boot.initrd.luks.devices."nixos-root".device =
-    "/dev/disk/by-uuid/ec1f1df4-349f-43ca-aa22-8c60a5845a5a";
 
   fileSystems."/home" = {
     device = "/dev/disk/by-uuid/f313ecbb-7133-4a4e-bf77-272570e8a286";
@@ -41,19 +46,46 @@
     fsType = "vfat";
   };
 
-  fileSystems."/run/media/moritz/1TBSSD" = {
-    device = "/dev/disk/by-uuid/06113bad-6b63-4c3c-9816-998ec6b0a994";
-    fsType = "ext4";
+  # Crucial CT1000P1SSD8
+
+  boot.initrd.luks.devices."games" = {
+    device = "/dev/disk/by-uuid/b66a028b-9986-4c08-b674-bb2c5129620b";
+    bypassWorkqueues = true;
+    allowDiscards = true;
   };
 
-  fileSystems."/run/media/moritz/1TBHDD" = {
-    device = "/dev/disk/by-uuid/07f200d9-153b-4396-a6e6-cf208a154435";
-    fsType = "ext4";
+  fileSystems."/run/media/moritz/games" = {
+    device = "/dev/disk/by-uuid/8b85ed66-baa2-42b4-b179-c0f6aaa2a555";
+    fsType = "btrfs";
+    options = [ "compress=zstd:1" "noatime" ];
+  };
+
+  # KINGSTON SA2000M8500G
+
+  boot.initrd.luks.devices."500GBSSD" = {
+    device = "/dev/disk/by-uuid/a3fbfddd-fbe9-4b6f-bc08-579810b18bae";
+    bypassWorkqueues = true;
+    allowDiscards = true;
   };
 
   fileSystems."/run/media/moritz/500GBSSD" = {
-    device = "/dev/disk/by-uuid/eeb62038-60d0-4c6b-ba90-1258728b8f66";
+    device = "/dev/disk/by-uuid/2c8e25fe-d15d-44d5-bd9e-024f1e9a8fe0";
     fsType = "ext4";
+    options = [ "noatime" ];
+  };
+
+  # Samsung SSD 870 QVO 2TB
+
+  boot.initrd.luks.devices."archive" = {
+    device = "/dev/disk/by-uuid/ff1db63b-e1b0-494e-8425-f20d23e9ffc1";
+    bypassWorkqueues = true;
+    allowDiscards = true;
+  };
+
+  fileSystems."/run/media/moritz/archive" = {
+    device = "/dev/disk/by-uuid/e86999be-1893-440c-80e9-9cee8af2fada";
+    fsType = "ext4";
+    options = [ "noatime" ];
   };
 
   swapDevices = [{ device = "/swap/swapfile"; }];
