16eacc53a0684a59173530ea9c1e8b7eb448cdd9
Moritz Vogel <moritzv7@gmail.com>
2023-09-07 15:21:19 +0200

    Improve the playerctl-current.sh script

--- packages/playerctl-current/playerctl-current.sh
+++ packages/playerctl-current/playerctl-current.sh
@@ -1,32 +1,33 @@
 #!/usr/bin/env bash
 
-# FIXME: Remove this
-set +o nounset
+main() {
+    IFS=$'\n'
 
-IFS=$'\n'
+    command -v playerctl &> /dev/null || exit 1
+    [[ -z "$XDG_RUNTIME_DIR" ]] && exit 1
 
-command -v playerctl &> /dev/null || exit 1
-[[ -z "$XDG_RUNTIME_DIR" ]] && exit 1
+    TMP_DIR="${XDG_RUNTIME_DIR}/playerctl-current"
+    [[ -d "$TMP_DIR" ]] || mkdir -p "$TMP_DIR"
+    PLAYER_FILE="$TMP_DIR/last_player"
+    [[ -e "$PLAYER_FILE" ]] || touch "$PLAYER_FILE"
 
-TMP_DIR="${XDG_RUNTIME_DIR}/playerctl-current"
-[[ -d "$TMP_DIR" ]] || mkdir -p "$TMP_DIR"
-PLAYER_FILE="$TMP_DIR/last_player"
-[[ -e "$PLAYER_FILE" ]] || touch "$PLAYER_FILE"
+    players=("$(playerctl --list-all)")
 
-players=$(playerctl --list-all)
+    selected_player=""
 
-# FIXME: fix shellcheck error
-# shellcheck disable=SC2068
-for player in ${players[@]} ; do
-    current_status="$(playerctl --player "$player" status 2>&1)"
-    if [[ "$current_status" = "Playing" ]]; then
-        selected_player="$player"
-        echo -n "$selected_player" > "$PLAYER_FILE"
-        break
-    fi
-done
+    for player in "${players[@]}"; do
+        current_status="$(playerctl --player "$player" status 2>&1)"
+        if [[ "$current_status" = "Playing" ]]; then
+            selected_player="$player"
+            echo -n "$selected_player" > "$PLAYER_FILE"
+            break
+        fi
+    done
 
-[[ -z "$selected_player" ]] && selected_player=$(<"$PLAYER_FILE")
-[[ -z "$selected_player" ]] && selected_player="${players[0]}"
+    [[ -z "$selected_player" ]] && selected_player=$(<"$PLAYER_FILE")
+    [[ -z "$selected_player" ]] && selected_player="${players[0]}"
 
-playerctl --player "$selected_player" "$@"
+    playerctl --player "$selected_player" "$@"
+}
+
+{ main "$@"; exit "$?"; }
