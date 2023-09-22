a698eb768b4f8c3dfe088ed41c52292fddc38552
Moritz Vogel <moritzv7@gmail.com>
2023-09-23 01:08:40 +0200

    Put steam into firejail

--- nixos-modules/firejail/default.nix
+++ nixos-modules/firejail/default.nix
@@ -9,6 +9,16 @@
         executable = "${pkgs.prismlauncher}/bin/prismlauncher";
         profile = ./prismlauncher.profile;
       };
+      steam = {
+        executable = "${pkgs.steam}/bin/steam";
+        # The string apparently gets executed using bash by nix, so the string interpolation
+        # has to be escaped two times.
+        extraArgs = let steam_home = "\\\${HOME}/.local/share/steam";
+        # "--mkdir" gets executed both in the real filesystem and in the private fs.
+        # Is it possible to only execute it on the real fs?
+        # See https://github.com/netblue30/firejail/issues/903
+        in [ "--noprofile" "--mkdir=${steam_home}" "--private=${steam_home}" ];
+      };
     };
   };
 }
