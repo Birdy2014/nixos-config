4a3bc4960b21b8ff1b6e6275077cf9e0b1421d35
Moritz Vogel <moritzv7@gmail.com>
2023-10-22 15:15:27 +0200

    soundblaster: Make workaround more robust

--- hosts/rotkehlchen/soundblaster.nix
+++ hosts/rotkehlchen/soundblaster.nix
@@ -2,25 +2,7 @@
 
 {
   # Workaround for Creative Sound BlasterX G6
-  systemd = let
-    # Wait until wireplumber has set the alsa options. This takes a while especially after suspend.
-    amixerWaitScript = ''
-      ${pkgs.coreutils-full}/bin/sleep 10
-    '';
-
-    alsaOptionsScript = ''
-      ${pkgs.alsaUtils}/bin/amixer -D hw:G6 sset 'PCM Capture Source' 'External Mic'
-      ${pkgs.alsaUtils}/bin/amixer -D hw:G6 sset 'External Mic',0 Capture cap
-      ${pkgs.alsaUtils}/bin/amixer -D hw:G6 sset 'External Mic',0 Capture 9dB
-      ${pkgs.alsaUtils}/bin/amixer -D hw:G6 sset 'Line In',0 Capture nocap
-      ${pkgs.alsaUtils}/bin/amixer -D hw:G6 sset 'S/PDIF In',0 Capture nocap
-      ${pkgs.alsaUtils}/bin/amixer -D hw:G6 sset 'What U Hear',0 Capture nocap
-
-      ${pkgs.alsaUtils}/bin/amixer -D hw:G6 sset 'Line In',0 Playback mute
-      ${pkgs.alsaUtils}/bin/amixer -D hw:G6 sset 'External Mic',0 Playback mute
-      ${pkgs.alsaUtils}/bin/amixer -D hw:G6 sset 'S/PDIF In',0 Playback mute
-    '';
-  in {
+  systemd = {
     services.soundblaster-reset-usb = {
       script =
         "${pkgs.usb-modeswitch}/bin/usb_modeswitch -v 0x041e -p 0x3256 --reset-usb";
@@ -29,27 +11,25 @@
     };
 
     user.services.soundblaster-set-alsa-options = {
-      script = alsaOptionsScript;
-      preStart = amixerWaitScript;
-      wantedBy = [ "pipewire.service" ];
+      script = ''
+        ${pkgs.alsaUtils}/bin/amixer -D hw:G6 sset 'PCM Capture Source' 'External Mic'
+        ${pkgs.alsaUtils}/bin/amixer -D hw:G6 sset 'External Mic',0 Capture cap
+        ${pkgs.alsaUtils}/bin/amixer -D hw:G6 sset 'External Mic',0 Capture 9dB
+        ${pkgs.alsaUtils}/bin/amixer -D hw:G6 sset 'Line In',0 Capture nocap
+        ${pkgs.alsaUtils}/bin/amixer -D hw:G6 sset 'S/PDIF In',0 Capture nocap
+        ${pkgs.alsaUtils}/bin/amixer -D hw:G6 sset 'What U Hear',0 Capture nocap
+
+        ${pkgs.alsaUtils}/bin/amixer -D hw:G6 sset 'Line In',0 Playback mute
+        ${pkgs.alsaUtils}/bin/amixer -D hw:G6 sset 'External Mic',0 Playback mute
+        ${pkgs.alsaUtils}/bin/amixer -D hw:G6 sset 'S/PDIF In',0 Playback mute
+      '';
+      # Wait until wireplumber has set the alsa options.
+      preStart = "${pkgs.coreutils-full}/bin/sleep 5";
+      partOf = [ "sound.target" ];
+      # pipewire.service reached on initial startup sound.target is reached on subsequent soundcard events
+      wantedBy = [ "sound.target" "pipewire.service" ];
       after = [ "pipewire.service" "wireplumber.service" ];
       serviceConfig = { Type = "oneshot"; };
-    };
-
-    services.soundblaster-set-alsa-options = {
-      script = alsaOptionsScript;
-      preStart = amixerWaitScript;
-      wantedBy = [
-        "suspend.target"
-        "hibernate.target"
-        "hybrid-sleep.target"
-        "suspend-then-hibernate.target"
-      ];
-      serviceConfig = {
-        Type = "oneshot";
-        User = "moritz";
-        Group = "users";
-      };
     };
   };
 
