8b0453b24a00021a6ddba3ed0b335300a7286721
Moritz Vogel <moritzv7@gmail.com>
2023-11-15 21:57:33 +0100

    Add `imv` to home-manager

---
+++ home/imv.nix
@@ -0,0 +1,21 @@
+{ ... }:
+
+{
+  programs.imv = {
+    enable = true;
+
+    settings = {
+      options.overlay_font = "monospace:10";
+      binds = {
+        "<Shift+K>" = "prev";
+        "<Shift+J>" = "next";
+
+        "o" = "";
+        "i" = "overlay";
+        "d" = "";
+        "<plus>" = "zoom 1";
+        "<minus>" = "zoom -1";
+      };
+    };
+  };
+}
