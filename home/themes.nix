0528ed2b83c70cffc2f57ae66a3021383fd2be2e
Moritz Vogel <moritzv7@gmail.com>
2023-11-28 18:15:38 +0100

    gtk: Add missing colors

--- home/themes.nix
+++ home/themes.nix
@@ -36,40 +36,74 @@
       "file://${config.home.homeDirectory}/Videos"
     ];
 
+    # Reference: https://gnome.pages.gitlab.gnome.org/libadwaita/doc/main/named-colors.html
     gtk4.extraCss = ''
+      /* Accent */
       @define-color accent_color #d4be98;
       @define-color accent_bg_color #282828;
       @define-color accent_fg_color #d4be98;
+
+      /* Destructive */
       @define-color destructive_color #ff7b63;
       @define-color destructive_bg_color #c01c28;
       @define-color destructive_fg_color #ffffff;
+
+      /* Success */
       @define-color success_color #a9b665;
       @define-color success_bg_color #26a269;
       @define-color success_fg_color #ffffff;
+
+      /* Warning */
       @define-color warning_color #d8a657;
       @define-color warning_bg_color #cd9309;
       @define-color warning_fg_color rgba(0, 0, 0, 0.8);
+
+      /* Error */
       @define-color error_color #ea6962;
       @define-color error_bg_color #e33e35;
       @define-color error_fg_color #ffffff;
+
+      /* Window */
       @define-color window_bg_color #282828;
       @define-color window_fg_color #dbc8a9;
+
+      /* View */
       @define-color view_bg_color #1e1e1e;
       @define-color view_fg_color @window_fg_color;
+
+      /* Headerbar */
       @define-color headerbar_bg_color #32302f;
       @define-color headerbar_fg_color @window_fg_color;
       @define-color headerbar_border_color #D4BE98;
       @define-color headerbar_backdrop_color #282625;
       @define-color headerbar_shade_color rgba(0, 0, 0, 0.36);
-      @define-color card_bg_color rgba(255, 255, 255, 0.08);
+
+      /* Card */
+      @define-color card_bg_color @dialog_bg_color;
       @define-color card_fg_color @window_fg_color;
       @define-color card_shade_color rgba(0, 0, 0, 0.36);
+
+      /* Dialog */
       @define-color dialog_bg_color #383838;
       @define-color dialog_fg_color #ffffff;
-      @define-color popover_bg_color #383838;
+
+      /* Popover */
+      @define-color popover_bg_color @dialog_bg_color;
       @define-color popover_fg_color #ffffff;
+
+      /* Misc */
       @define-color shade_color rgba(0, 0, 0, 0.36);
       @define-color scrollbar_outline_color rgba(0, 0, 0, 0.5);
+
+      /* Sidebar */
+      @define-color sidebar_bg_color @window_bg_color;
+      @define-color sidebar_fg_color @window_fg_color;
+      @define-color sidebar_backdrop_color #2a2a2a;
+      @define-color sidebar_shade_color rgba(0, 0, 0, 0.36);
+      @define-color secondary_sidebar_bg_color #2a2a2a;
+      @define-color secondary_sidebar_fg_color @sidebar_fg_color;
+      @define-color secondary_sidebar_backdrop_color #272727;
+      @define-color secondary_sidebar_shade_color @sidebar_shade_color;
 
       /* Remove close button */
       windowcontrols * {
