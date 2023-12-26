c97b44b86299d70a00c4ef4f14b0dc5a4501c588
Moritz Vogel <moritzv7@gmail.com>
2023-12-26 03:04:58 +0100

    lepton-firefox-theme: 8.5.0 -> 8.5.1

--- packages/lepton-firefox-theme.nix
+++ packages/lepton-firefox-theme.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   pname = "lepton-firefox-theme";
-  version = "8.5.0";
+  version = "8.5.1";
 
   src = fetchFromGitHub {
     owner = "black7375";
     repo = "Firefox-UI-Fix";
     rev = "v${version}";
-    hash = "sha256-wv5EH3osPZNGe66hIJEjIy8rZAiFxpAzGatm/YgcF3o=";
+    hash = "sha256-qiJmA25HVw/IOQictcnhTxXfbUWQ11TTEcQTEzIeMug=";
   };
 
   installPhase = ''
