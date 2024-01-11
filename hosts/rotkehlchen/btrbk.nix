fd2f2672952e35ad264843d6cdeb822bd8096fc5
Moritz Vogel <moritzv7@gmail.com>
2024-01-11 19:00:00 +0100

    rotkehlchen: Snapshot everything

--- hosts/rotkehlchen/btrbk.nix
+++ hosts/rotkehlchen/btrbk.nix
@@ -1,10 +1,37 @@
 { ... }:
 
 {
-  services.btrbk.instances.home.settings = {
-    snapshot_preserve = "14d";
-    snapshot_preserve_min = "2d";
-    snapshot_dir = "/snapshots";
-    subvolume."/home" = { };
+  services.btrbk.instances = {
+    root.settings = {
+      snapshot_preserve = "14d";
+      snapshot_preserve_min = "2d";
+      snapshot_dir = "/snapshots";
+      subvolume."/" = { };
+      subvolume."/home" = { };
+    };
+
+    archive.settings = let mountpoint = "/run/media/moritz/archive";
+    in {
+      snapshot_preserve = "14d";
+      snapshot_preserve_min = "2d";
+      snapshot_dir = "${mountpoint}/snapshots";
+      subvolume."${mountpoint}" = { };
+    };
+
+    archive2.settings = let mountpoint = "/run/media/moritz/archive2";
+    in {
+      snapshot_preserve = "14d";
+      snapshot_preserve_min = "2d";
+      snapshot_dir = "${mountpoint}/snapshots";
+      subvolume."${mountpoint}" = { };
+    };
+
+    games.settings = let mountpoint = "/run/media/moritz/games";
+    in {
+      snapshot_preserve = "14d";
+      snapshot_preserve_min = "2d";
+      snapshot_dir = "${mountpoint}/snapshots";
+      subvolume."${mountpoint}" = { };
+    };
   };
 }
