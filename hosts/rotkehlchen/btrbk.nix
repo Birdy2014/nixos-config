39cf7922b0e65081780914708fe62f7b8df836a0
Moritz Vogel <moritzv7@gmail.com>
2024-01-15 17:05:55 +0100

    rotkehlchen: Add hourly snapshots

--- hosts/rotkehlchen/btrbk.nix
+++ hosts/rotkehlchen/btrbk.nix
@@ -2,36 +2,30 @@
 
 {
   services.btrbk.instances = {
-    root.settings = {
-      snapshot_preserve = "14d";
-      snapshot_preserve_min = "2d";
-      snapshot_dir = "/snapshots";
-      subvolume."/" = { };
-      subvolume."/home" = { };
+    root = {
+      settings = {
+        snapshot_preserve = "14d 4h";
+        snapshot_preserve_min = "2h";
+        snapshot_dir = "/snapshots";
+        subvolume."/" = { };
+        subvolume."/home" = { };
+      };
+      onCalendar = "hourly";
     };
-
-    archive.settings = let mountpoint = "/run/media/moritz/archive";
-    in {
-      snapshot_preserve = "14d";
-      snapshot_preserve_min = "2d";
-      snapshot_dir = "${mountpoint}/snapshots";
-      subvolume."${mountpoint}" = { };
+  } // (builtins.listToAttrs (map (mountpoint: {
+    name = baseNameOf mountpoint;
+    value = {
+      settings = {
+        snapshot_preserve = "14d 4h";
+        snapshot_preserve_min = "2h";
+        snapshot_dir = "${mountpoint}/snapshots";
+        subvolume."${mountpoint}" = { };
+      };
+      onCalendar = "hourly";
     };
-
-    archive2.settings = let mountpoint = "/run/media/moritz/archive2";
-    in {
-      snapshot_preserve = "14d";
-      snapshot_preserve_min = "2d";
-      snapshot_dir = "${mountpoint}/snapshots";
-      subvolume."${mountpoint}" = { };
-    };
-
-    games.settings = let mountpoint = "/run/media/moritz/games";
-    in {
-      snapshot_preserve = "14d";
-      snapshot_preserve_min = "2d";
-      snapshot_dir = "${mountpoint}/snapshots";
-      subvolume."${mountpoint}" = { };
-    };
-  };
+  }) [
+    "/run/media/moritz/archive"
+    "/run/media/moritz/archive2"
+    "/run/media/moritz/games"
+  ]));
 }
