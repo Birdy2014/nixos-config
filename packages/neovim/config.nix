f662bed22257e0dcf7ed8d92998bbdbce24520f6
Moritz Vogel <moritzv7@gmail.com>
2024-01-24 00:38:55 +0100

    neovim: Remove `satellite.nvim`

--- packages/neovim/config.nix
+++ packages/neovim/config.nix
@@ -29,19 +29,6 @@
       config = ./config/plugins/dressing.lua;
     }
     {
-      # satellite.nvim is only compatible with the latest nightly version of neovim
-      plugin = satellite-nvim.overrideAttrs {
-        version = "2023-05-25";
-        src = fetchFromGitHub {
-          owner = "lewis6991";
-          repo = "satellite.nvim";
-          rev = "022c884978b888d5b5812052c64d0d243092155e";
-          sha256 = "sha256-8S9MLedEq9O8HJx8n7yFS5gKn8Ap7SOLMf45eGIzbUQ=";
-        };
-      };
-      config = ''require("satellite").setup()'';
-    }
-    {
       plugin = [ alpha-nvim nvim-web-devicons ];
       config = ./config/plugins/alpha.lua;
     }
