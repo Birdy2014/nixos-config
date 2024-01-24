5bdce1b95c8aab20002817291b214a5ef41ee4ef
Moritz Vogel <moritzv7@gmail.com>
2024-01-24 16:50:37 +0100

    neovim: Add keymaps to create splits

--- packages/neovim/config/keymap.lua
+++ packages/neovim/config/keymap.lua
@@ -8,3 +8,8 @@ vim.keymap.set({"n", "v"}, "Ä", "}", { remap = true })
 vim.keymap.set("n", "<esc>", "<cmd>noh<cr>", { desc = "Hide search highlight" })
 vim.keymap.set("n", "k", "(v:count == 0 ? 'gk' : 'k')", { expr = true })
 vim.keymap.set("n", "j", "(v:count == 0 ? 'gj' : 'j')", { expr = true })
+
+vim.keymap.set("n", "<leader>sh", "<cmd>vs<cr>", { desc = "Split Left" })
+vim.keymap.set("n", "<leader>sj", "<cmd>sp<cr>", { desc = "Split Down" })
+vim.keymap.set("n", "<leader>sk", "<cmd>sp<cr>", { desc = "Split Up" })
+vim.keymap.set("n", "<leader>sl", "<cmd>vs<cr>", { desc = "Split Right" })
