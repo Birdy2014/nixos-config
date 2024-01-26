fe0ec9d8f9f62cad27738ddd12ba7fb164777e86
Moritz Vogel <moritzv7@gmail.com>
2024-01-26 17:09:19 +0100

    podman: Remove dns setting

--- nixos-modules/virtualisation.nix
+++ nixos-modules/virtualisation.nix
@@ -10,8 +10,6 @@ in {
       podman = {
         enable = true;
         dockerCompat = true;
-        # Required for containers under podman-compose to be able to talk to each other.
-        defaultNetwork.settings.dns_enabled.enable = true;
       };
 
       libvirtd = {
