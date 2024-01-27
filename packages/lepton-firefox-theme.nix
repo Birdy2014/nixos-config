3b116210a8b75c44de04b0b22f1a2e518936f5bd
Moritz Vogel <moritzv7@gmail.com>
2024-01-28 00:36:01 +0100

    lepton-firefox-theme: Add patch to fix compatibility with Firefox 122

--- packages/lepton-firefox-theme.nix
+++ packages/lepton-firefox-theme.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchFromGitHub }:
+{ stdenv, fetchFromGitHub, fetchpatch }:
 
 stdenv.mkDerivation rec {
   pname = "lepton-firefox-theme";
@@ -10,6 +10,16 @@ stdenv.mkDerivation rec {
     rev = "v${version}";
     hash = "sha256-qiJmA25HVw/IOQictcnhTxXfbUWQ11TTEcQTEzIeMug=";
   };
+
+  patches = [
+    # See https://github.com/black7375/Firefox-UI-Fix/issues/864
+    # Remove with next release
+    (fetchpatch {
+      url =
+        "https://github.com/black7375/Firefox-UI-Fix/commit/4545b12bbcedca327b4692c8e73685c33669237f.diff";
+      hash = "sha256-g7P7dTkjxYf1ONCX7oATDrJjKwq6sVrFHkPLkwQLqJE=";
+    })
+  ];
 
   installPhase = ''
     cp -r . "$out"
