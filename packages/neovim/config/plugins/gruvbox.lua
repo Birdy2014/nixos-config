98e0c357067d7d1a02b9bfbf92353ba68f85a322
Moritz Vogel <moritzv7@gmail.com>
2024-02-16 15:45:41 +0100

    neovim: Remove obsolete inlay hint highlight

--- packages/neovim/config/plugins/gruvbox.lua
+++ packages/neovim/config/plugins/gruvbox.lua
@@ -9,7 +9,4 @@ vim.defer_fn(function()
     vim.g.terminal_color_5 = "#b16286"
     vim.g.terminal_color_6 = "#689d6a"
     vim.g.terminal_color_7 = "#a89984"
-
-    -- TODO: inlay hint
-    -- vim.cmd[[highlight link LspInlayHint Comment']]
 end, 0)
