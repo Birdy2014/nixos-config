9d70b2587874f67cd6fc231378b6711848961d85
Moritz Vogel <moritzv7@gmail.com>
2024-02-28 00:02:52 +0100

    lepton-firefox-theme: 8.5.1 -> 8.6.0

--- packages/lepton-firefox-theme.nix
+++ packages/lepton-firefox-theme.nix
@@ -1,25 +1,15 @@
-{ stdenv, fetchFromGitHub, fetchpatch }:
+{ stdenv, fetchFromGitHub }:
 
 stdenv.mkDerivation rec {
   pname = "lepton-firefox-theme";
-  version = "8.5.1";
+  version = "8.6.0";
 
   src = fetchFromGitHub {
     owner = "black7375";
     repo = "Firefox-UI-Fix";
     rev = "v${version}";
-    hash = "sha256-qiJmA25HVw/IOQictcnhTxXfbUWQ11TTEcQTEzIeMug=";
+    hash = "sha256-+TNEtdYePMVBLRUJyzZOKgzFr8tqfqmxy8NfaABP1jw=";
   };
-
-  patches = [
-    # See https://github.com/black7375/Firefox-UI-Fix/issues/864
-    # Remove with next release
-    (fetchpatch {
-      url =
-        "https://github.com/black7375/Firefox-UI-Fix/commit/4545b12bbcedca327b4692c8e73685c33669237f.diff";
-      hash = "sha256-g7P7dTkjxYf1ONCX7oATDrJjKwq6sVrFHkPLkwQLqJE=";
-    })
-  ];
 
   installPhase = ''
     cp -r . "$out"
