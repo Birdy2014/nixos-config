b379501778175f24fac9d55dc9925cf2f70b74a9
Moritz Vogel <moritzv7@gmail.com>
2024-03-18 00:26:10 +0100

    Add fcitx5 settings

--- nixos-modules/desktop.nix
+++ nixos-modules/desktop.nix
@@ -80,7 +80,32 @@
 
     i18n.inputMethod = {
       enabled = "fcitx5";
-      fcitx5.addons = with pkgs; [ fcitx5-mozc fcitx5-gtk ];
+      fcitx5 = {
+        addons = with pkgs; [ fcitx5-mozc fcitx5-gtk ];
+        settings = {
+          inputMethod = {
+            "Groups/0" = {
+              Name = "Default";
+              "Default Layout" = "de-nodeadkeys";
+              DefaultIM = "mozc";
+            };
+            "Groups/0/Items/0" = {
+              Name = "keyboard-de-nodeadkeys";
+              Layout = "";
+            };
+            "Groups/0/Items/1" = {
+              Name = "mozc";
+              Layout = "";
+            };
+            "GroupOrder"."0" = "Default";
+          };
+          globalOptions = {
+            "Hotkey/TriggerKeys" = { "0" = "Super_R"; };
+            # waylandim is currently broken with sway
+            "Behavior/DisabledAddons"."0" = "waylandim";
+          };
+        };
+      };
     };
 
     services.syncthing = {
