188fd608d4c501023bf98b80586038c7bda07bd6
Moritz Vogel <moritzv7@gmail.com>
2024-04-06 03:09:20 +0200

    waybar: Upgrade to fix style issue in `sway/workspaces`

--- overlays/waybar.nix
+++ overlays/waybar.nix
@@ -1,13 +1,12 @@
 # TODO: Remove when waybar 0.10.1 is released
 
 final: prev: {
-  waybar = prev.waybar.overrideAttrs (old: {
-    patches = (old.patches or [ ]) ++ [
-      (final.fetchpatch {
-        url =
-          "https://github.com/Alexays/Waybar/commit/2ffd9a94a505a2e7e933ea8303f9cf2af33c35fe.diff";
-        hash = "sha256-u87t6zzslk1mzSfi4HQ6zDPFr7qMfsvymTy3HBxVTJQ=";
-      })
-    ];
-  });
+  waybar = (prev.waybar.overrideAttrs (old: {
+    src = final.fetchFromGitHub {
+      owner = "Alexays";
+      repo = "Waybar";
+      rev = "3de9e0cbd32c090daedf81fc86b2bf88e060fbce";
+      hash = "sha256-gVVRjd1IL0NAbBLiWJAl1xqkX8dLTS+z3xjJFfGaTIk=";
+    };
+  })).override { wireplumberSupport = false; };
 }
