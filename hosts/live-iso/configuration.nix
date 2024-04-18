c221b1c85bfefb2a9e51e13524fa9d2850965caf
Moritz Vogel <moritzv7@gmail.com>
2024-04-18 23:26:54 +0200

    live-iso: Add default sway wallpaper

--- hosts/live-iso/configuration.nix
+++ hosts/live-iso/configuration.nix
@@ -27,4 +27,14 @@
   services.getty.autologinUser = lib.mkForce "moritz";
 
   environment.systemPackages = [ pkgs.gparted pkgs.glxinfo ];
+
+  home-manager.users.moritz.wayland.windowManager.sway.extraConfig = ''
+    output * bg ${
+      pkgs.fetchurl {
+        url =
+          "https://raw.githubusercontent.com/NixOS/nixos-artwork/c68a508b95baa0fcd99117f2da2a0f66eb208bbf/wallpapers/nix-wallpaper-gear.png";
+        hash = "sha256-2sT6b49/iClTs9QuUvpmZ5gcIeXI9kebs5IqgQN1RL8=";
+      }
+    } fill
+  '';
 }
