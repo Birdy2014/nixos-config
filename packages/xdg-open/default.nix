f54a0068cafda3aa0e100080f2b759a7fb742b3a
Moritz Vogel <moritzv7@gmail.com>
2024-05-13 11:39:40 +0200

    Wrap custom xdg-open to fix sharing with kdeconnect

--- packages/xdg-open/default.nix
+++ packages/xdg-open/default.nix
@@ -1,12 +1,19 @@
-{ stdenv, python3, ... }:
+{ lib, stdenv, makeWrapper, coreutils, python3, ... }:
 
 stdenv.mkDerivation {
   pname = "xdg-open";
   version = "0.0.1";
 
-  propagatedBuildInputs = [ python3 ];
+  nativeBuildInputs = [ makeWrapper ];
+  buildInputs = [ python3 ];
   dontUnpack = true;
-  installPhase = "install -Dm755 ${./xdg-open.py} $out/bin/xdg-open";
+
+  installPhase = ''
+    install -Dm755 ${./xdg-open.py} $out/bin/xdg-open
+
+    wrapProgram $out/bin/xdg-open \
+      --prefix PATH : ${lib.makeBinPath [ coreutils ]}
+  '';
 
   meta = {
     description = "My xdg-open replacement";
