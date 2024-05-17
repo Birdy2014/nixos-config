14d91c51f385fc2cec4e3f688fef1c0633750505
Moritz Vogel <moritzv7@gmail.com>
2024-05-17 02:53:53 +0200

    Remove dolphin file manager

--- home/misc.nix
+++ home/misc.nix
@@ -21,9 +21,6 @@
     keepassxc
     zathura
     mcomix
-    dolphin
-    libsForQt5.kio # needed for the SMB password prompt in dolphin
-    libsForQt5.kio-extras # for accessing SMB shares
     neovide
 
     libreoffice
