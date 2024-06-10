be97337cda8b0e91e5aa2ec9a922288a6a24d561
Moritz Vogel <moritzv7@gmail.com>
2024-06-10 21:28:19 +0200

    Add script to call nixos-rebuild with custom options

---
+++ scripts/rebuild.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+if [[ "$(hostname)" != 'rotkehlchen' ]]; then
+    echo "This script should only be executed on rotkehlchen" >&2
+    exit 1
+fi
+
+if [[ "$#" -lt 2 ]]; then
+    echo "Usage: $0 <test|boot|switch> <hostnames...>" >&2
+    exit 1
+fi
+
+mode="$1"
+
+case "$mode" in
+    test|boot|switch)
+        ;;
+    *)
+        echo "Invalid mode '$mode'" >&2
+        exit 1
+        ;;
+esac
+
+shift
+
+flake_path='/etc/nixos'
+
+declare -a commands
+
+for host in "$@"; do
+    case "$host" in
+        rotkehlchen)
+            commands+=("sudo nixos-rebuild $mode --flake $flake_path#rotkehlchen")
+            ;;
+        seidenschwanz)
+            commands+=("ssh seidenschwanz git -C /etc/nixos-secrets pull")
+            commands+=("nixos-rebuild $mode --flake $flake_path#seidenschwanz --target-host seidenschwanz")
+            ;;
+        buntspecht)
+            commands+=("ssh buntspecht git -C /etc/nixos-secrets pull")
+            commands+=("nixos-rebuild $mode --flake $flake_path#buntspecht --target-host buntspecht --build-host buntspecht --use-substitutes")
+            ;;
+        zilpzalp)
+            commands+=("nixos-rebuild $mode --flake $flake_path#zilpzalp --target-host root@zilpzalp")
+            ;;
+        *)
+            echo "Invalid hostname '$host'" >&2
+            exit 1
+            ;;
+    esac
+done
+
+for command in "${commands[@]}"; do
+    echo "Executing '$command'..."
+    $command
+    echo
+done
