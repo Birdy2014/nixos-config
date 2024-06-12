7137c6cc780a203dc570b8d9bf6c0f2c25da252e
Moritz Vogel <moritzv7@gmail.com>
2024-06-13 01:27:58 +0200

    lf: Add patch to allow paths in desktop files

--- overlays/lf/default.nix
+++ overlays/lf/default.nix
@@ -1,6 +1,8 @@
 final: prev: {
   lf = prev.lf.overrideAttrs (old: {
-    patches = (old.patches or [ ])
-      ++ [ ./0001-Show-dirsize-if-dircounts-is-enabled.patch ];
+    patches = (old.patches or [ ]) ++ [
+      ./0001-Show-dirsize-if-dircounts-is-enabled.patch
+      ./0002-Add-file-path-to-exec-in-desktop-file.patch
+    ];
   });
 }
