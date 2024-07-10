c720cf7a75567bf7f1574633c2af7e2065902c16
Moritz Vogel <moritzv7@gmail.com>
2024-07-10 21:06:17 +0200

    Downgrade neovim to the version in nixos-24.05
    Neovim broke sometimes with errors regarding extmarks while an lsp server
    was active. I hope that this downgrade solves this problem.

--- packages/default.nix
+++ packages/default.nix
@@ -1,27 +1,24 @@
 { nixpkgs, nixpkgs-unstable }:
 
 {
-  x86_64-linux = let
-    callPackage = nixpkgs.legacyPackages.x86_64-linux.callPackage;
-    callPackageUnstable =
-      nixpkgs-unstable.legacyPackages.x86_64-linux.callPackage;
-  in {
-    gruvbox-material-gtk = callPackage ./gruvbox-material-gtk.nix { };
-    improtheater-frankfurt = callPackage ./improtheater-frankfurt.nix { };
-    lepton-firefox-theme = callPackage ./lepton-firefox-theme.nix { };
-    lyrax-cursors = callPackage ./lyrax-cursors.nix { };
-    mpv-thumbfast-vanilla-osc = callPackage ./mpv-thumbfast-vanilla-osc.nix { };
-    neovim = callPackageUnstable ./neovim { };
-    playerctl-current = callPackage ./playerctl-current { };
-    xdg-open = callPackage ./xdg-open { };
-  };
+  x86_64-linux =
+    let callPackage = nixpkgs.legacyPackages.x86_64-linux.callPackage;
+    in {
+      gruvbox-material-gtk = callPackage ./gruvbox-material-gtk.nix { };
+      improtheater-frankfurt = callPackage ./improtheater-frankfurt.nix { };
+      lepton-firefox-theme = callPackage ./lepton-firefox-theme.nix { };
+      lyrax-cursors = callPackage ./lyrax-cursors.nix { };
+      mpv-thumbfast-vanilla-osc =
+        callPackage ./mpv-thumbfast-vanilla-osc.nix { };
+      neovim = callPackage ./neovim { };
+      playerctl-current = callPackage ./playerctl-current { };
+      xdg-open = callPackage ./xdg-open { };
+    };
 
-  aarch64-linux = let
-    callPackage = nixpkgs.legacyPackages.aarch64-linux.callPackage;
-    callPackageUnstable =
-      nixpkgs-unstable.legacyPackages.aarch64-linux.callPackage;
-  in {
-    improtheater-frankfurt = callPackage ./improtheater-frankfurt.nix { };
-    neovim = callPackageUnstable ./neovim { };
-  };
+  aarch64-linux =
+    let callPackage = nixpkgs.legacyPackages.aarch64-linux.callPackage;
+    in {
+      improtheater-frankfurt = callPackage ./improtheater-frankfurt.nix { };
+      neovim = callPackage ./neovim { };
+    };
 }
