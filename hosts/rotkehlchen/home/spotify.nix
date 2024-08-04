0eca3ef8b51f937e3bb9758879c1f36bde268210
Moritz Vogel <moritzv7@gmail.com>
2024-08-04 15:36:16 +0200

    Use a maintained fork of `spicetify-nix`

--- hosts/rotkehlchen/home/spotify.nix
+++ hosts/rotkehlchen/home/spotify.nix
@@ -1,12 +1,12 @@
 { pkgs, inputs, ... }:
 
-let spicePkgs = inputs.spicetify-nix.packages.${pkgs.system}.default;
+let spicePkgs = inputs.spicetify-nix.legacyPackages.${pkgs.system};
 in {
-  imports = [ inputs.spicetify-nix.homeManagerModule ];
+  imports = [ inputs.spicetify-nix.homeManagerModules.default ];
 
   programs.spicetify = {
     enable = true;
-    theme = spicePkgs.themes.Onepunch;
+    theme = spicePkgs.themes.onepunch;
     colorScheme = "dark";
 
     enabledExtensions = with spicePkgs.extensions; [
