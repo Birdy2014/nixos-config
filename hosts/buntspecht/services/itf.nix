e2fe70fb569dd0ee1a90e0b938a3a5c900c8b7b2
Moritz Vogel <moritzv7@gmail.com>
2024-08-25 18:36:44 +0200

    itf: Enable recommended proxy settings

--- hosts/buntspecht/services/itf.nix
+++ hosts/buntspecht/services/itf.nix
@@ -86,8 +86,10 @@ in {
       "${hostname}" = {
         enableACME = true;
         forceSSL = true;
-        locations."/".proxyPass =
-          "http://127.0.0.1:${builtins.toString itf-config.port}/";
+        locations."/" = {
+          proxyPass = "http://127.0.0.1:${builtins.toString itf-config.port}/";
+          recommendedProxySettings = true;
+        };
       };
 
       "www.${hostname}" = {
