ffe3f3a1837478157d06aa174c5f54fbc7e05d85
Moritz Vogel <moritzv7@gmail.com>
2024-08-29 20:52:26 +0200

    `last-updated.sh`: Fix leading zero being interpreted as octal

--- scripts/last-updated.sh
+++ scripts/last-updated.sh
@@ -13,14 +13,14 @@ timestamps="$(nix eval --json system#nixosConfigurations.rotkehlchen._module.spe
 echo Flake last updated on: "$(git -C /etc/nixos log -1 --pretty='format:%ar' flake.lock)"
 echo ---
 
-current_day="$(date '+%j')"
-current_hour="$(date '+%H')"
+current_day="10#$(date '+%j')"
+current_hour="10#$(date '+%H')"
 
 for i in $(seq 0 $(( $(echo "$timestamps" | jq length) - 1 )) ); do
     name="$(echo "$timestamps" | jq -r ".[$i].name")"
     timestamp="$(echo "$timestamps" | jq ".[$i].timestamp")"
-    day="$(date -d @"${timestamp}" '+%j')"
-    hour="$(date -d @"${timestamp}" '+%H')"
+    day="10#$(date -d @"${timestamp}" '+%j')"
+    hour="10#$(date -d @"${timestamp}" '+%H')"
     day_difference="$(( current_day - day ))"
     hour_difference="$(( current_hour - hour ))"
     echo -n "$name: "
