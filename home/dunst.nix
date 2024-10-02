c9ea38e3b50a2d82058172fdd07ec229327f7bad
Moritz Vogel <moritzv7@gmail.com>
2024-10-03 01:53:04 +0200

    dunst: Use system colorscheme

--- home/dunst.nix
+++ home/dunst.nix
@@ -1,38 +1,40 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
 
 {
   services.dunst = {
     enable = true;
 
-    settings = {
-      global = {
-        width = 300;
-        height = 200;
-        offset = "30x20";
-        frame_width = 1;
-        font = "monospace 10";
-        dmenu = "${pkgs.rofi}/bin/rofi -dmenu -p dunst";
-        format = "<b>%s</b>\\n%b";
-        markup = "full";
-      };
+    settings =
+      let inherit (config.my.theme) background-secondary text accent red;
+      in {
+        global = {
+          width = 300;
+          height = 200;
+          offset = "30x20";
+          frame_width = 1;
+          font = "monospace 10";
+          dmenu = "${pkgs.rofi}/bin/rofi -dmenu -p dunst";
+          format = "<b>%s</b>\\n%b";
+          markup = "full";
+        };
 
-      urgency_low = {
-        background = "#282828";
-        foreground = "#d4be98";
-        timeout = 10;
-      };
+        urgency_low = {
+          background = background-secondary;
+          foreground = text;
+          timeout = 10;
+        };
 
-      urgency_normal = {
-        background = "#282828";
-        foreground = "#a9b665";
-        timeout = 15;
-      };
+        urgency_normal = {
+          background = background-secondary;
+          foreground = accent;
+          timeout = 15;
+        };
 
-      urgency_critical = {
-        background = "#282828";
-        foreground = "#ea6962";
-        timeout = 0;
+        urgency_critical = {
+          background = background-secondary;
+          foreground = red;
+          timeout = 0;
+        };
       };
-    };
   };
 }
