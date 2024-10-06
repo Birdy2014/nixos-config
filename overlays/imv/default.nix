717ab4466be36351d5ac089c8866be5c38a457f1
Moritz Vogel <moritzv7@gmail.com>
2024-10-07 00:35:43 +0200

    imv: Fix gif support

--- overlays/imv/default.nix
+++ overlays/imv/default.nix
@@ -1,12 +1,16 @@
 final: prev: {
-  imv = prev.imv.overrideAttrs (old: {
+  imv = (prev.imv.overrideAttrs (old: {
     buildInputs = old.buildInputs ++ [ prev.libwebp ];
     patches = (old.patches or [ ]) ++ [
       ./0001-Set-cursor-image-to-left_ptr.patch
       ./0002-Add-support-for-webp.patch
       ./0003-Fix-distortion-in-heif-and-avif.patch
       ./0004-Add-qoi-backend.patch
+      ./0005-Update-libnsgif-support-for-libnsgif-1.0.0.patch
     ];
     mesonFlags = old.mesonFlags ++ [ "-Dlibwebp=enabled" ];
-  });
+  })).override {
+    withBackends =
+      [ "libjxl" "libtiff" "libjpeg" "libpng" "librsvg" "libheif" "libnsgif" ];
+  };
 }
