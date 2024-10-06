b981e96d48820868f5e30f1e1407aa9d7fa5d5fa
Moritz Vogel <moritzv7@gmail.com>
2024-10-07 00:41:50 +0200

    imv: Switch to my own fork

--- overlays/imv/default.nix
+++ overlays/imv/default.nix
@@ -1,13 +1,12 @@
 final: prev: {
   imv = (prev.imv.overrideAttrs (old: {
     buildInputs = old.buildInputs ++ [ prev.libwebp ];
-    patches = (old.patches or [ ]) ++ [
-      ./0001-Set-cursor-image-to-left_ptr.patch
-      ./0002-Add-support-for-webp.patch
-      ./0003-Fix-distortion-in-heif-and-avif.patch
-      ./0004-Add-qoi-backend.patch
-      ./0005-Update-libnsgif-support-for-libnsgif-1.0.0.patch
-    ];
+    src = prev.fetchFromGitHub {
+      owner = "Birdy2014";
+      repo = "imv";
+      rev = "3534c3f62b72529dc751878005d206510aa8c0d1";
+      hash = "sha256-fvlxUBkTEmr/vRAJ/Umc821rqqozsHRolHCoOyl1aNM=";
+    };
     mesonFlags = old.mesonFlags ++ [ "-Dlibwebp=enabled" ];
   })).override {
     withBackends =
