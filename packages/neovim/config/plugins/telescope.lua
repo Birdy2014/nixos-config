d6c90cb89247a4345b914bfa26f3ea2a335e9e3a
Moritz Vogel <moritzv7@gmail.com>
2024-10-08 16:58:12 +0200

    nvim: Change lsp_references picker

--- packages/neovim/config/plugins/telescope.lua
+++ packages/neovim/config/plugins/telescope.lua
@@ -3,7 +3,12 @@ require("telescope").setup {
         file_ignore_patterns = {
             "node_modules"
         },
-    }
+    },
+    pickers = {
+        lsp_references = {
+            theme = "cursor";
+        },
+    },
 }
 
 local builtin = require("telescope.builtin")
@@ -13,4 +18,4 @@ vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Find Buffers" })
 vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Find Help" })
 vim.keymap.set("n", "<leader>fs", builtin.lsp_dynamic_workspace_symbols, { desc = "Find LSP Symbols" })
 vim.keymap.set("n", "<leader>fm", function() builtin.man_pages({ sections = { "ALL" } }) end, { desc = "Find Man Pages" })
-vim.keymap.set("n", "<leader>fr", function() builtin.lsp_references({ jump_type = "never" }) end, { desc = "Find LSP References" })
+vim.keymap.set("n", "gh", function() builtin.lsp_references({ jump_type = "never" }) end, { desc = "Find LSP References" })
