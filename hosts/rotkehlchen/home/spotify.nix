f6389a15867f34f4996b144c319ab54bac4e5c42
Moritz Vogel <moritzv7@gmail.com>
2024-11-19 12:26:21 +0100

    Add spicetify colorschemes

--- hosts/rotkehlchen/home/spotify.nix
+++ hosts/rotkehlchen/home/spotify.nix
@@ -1,13 +1,30 @@
-{ pkgs, inputs, ... }:
+{ osConfig, pkgs, inputs, ... }:
 
 let spicePkgs = inputs.spicetify-nix.legacyPackages.${pkgs.system};
 in {
   imports = [ inputs.spicetify-nix.homeManagerModules.default ];
 
-  programs.spicetify = {
+  programs.spicetify = let
+    colorschemes = {
+      gruvbox-material-dark = {
+        theme = spicePkgs.themes.onepunch;
+        colorScheme = "dark";
+      };
+
+      catppuccin-macchiato = {
+        theme = spicePkgs.themes.catppuccin;
+        colorScheme = "macchiato";
+      };
+
+      catppuccin-frappe = {
+        theme = spicePkgs.themes.catppuccin;
+        colorScheme = "frappe";
+      };
+    };
+  in {
     enable = true;
-    theme = spicePkgs.themes.onepunch;
-    colorScheme = "dark";
+
+    inherit (colorschemes.${osConfig.my.desktop.colorscheme}) theme colorScheme;
 
     enabledExtensions = with spicePkgs.extensions; [
       fullAppDisplay
