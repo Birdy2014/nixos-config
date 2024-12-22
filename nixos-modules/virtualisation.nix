26964224e62735f6f5e244607ff8937c7f94c9ef
Moritz Vogel <moritzv7@gmail.com>
2024-12-22 20:49:09 +0100

    Downgrade libvirt to 10.8.0 to fix delay on shutdown

--- nixos-modules/virtualisation.nix
+++ nixos-modules/virtualisation.nix
@@ -15,6 +15,21 @@ in {
         enable = true;
         onBoot = "ignore";
 
+        # libvirt 10.9.0 delays shutdown for 5 seconds
+        # should be fixed with the next version
+        # https://bbs.archlinux.org/viewtopic.php?id=301825
+        # https://gitlab.com/libvirt/libvirt/-/issues/695
+        package = pkgs.libvirt.overrideAttrs {
+          version = "10.8.0";
+          src = pkgs.fetchFromGitLab {
+            owner = "libvirt";
+            repo = "libvirt";
+            rev = "v10.8.0";
+            hash = "sha256-MzfkpWvj0RTjrse/TzoFDfPdfQk6PkOx0CsTF99zveA=";
+            fetchSubmodules = true;
+          };
+        };
+
         qemu = {
           package = pkgs.qemu_kvm;
           ovmf.packages = [ pkgs.OVMFFull.fd ];
