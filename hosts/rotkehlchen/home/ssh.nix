96943d80ca6dba38ad512991dff94ddb1aea3359
Moritz Vogel <moritzv7@gmail.com>
2025-01-18 00:06:27 +0100

    rotkehlchen: Add ssh client config

---
+++ hosts/rotkehlchen/home/ssh.nix
@@ -0,0 +1,33 @@
+{ ... }:
+
+{
+  programs.ssh = {
+    enable = true;
+
+    serverAliveInterval = 20;
+
+    matchBlocks = {
+      seidenschwanz = {
+        hostname = "fd00:90::10";
+        user = "root";
+      };
+
+      seidenschwanz-initrd = {
+        hostname = "fd00:90::10";
+        user = "root";
+        port = 2222;
+      };
+
+      buntspecht = {
+        hostname = "2a01:4f8:c012:2dfe::1";
+        user = "root";
+        port = 46773;
+      };
+
+      zilpzalp = {
+        hostname = "zilpzalp.local";
+        user = "moritz";
+      };
+    };
+  };
+}
