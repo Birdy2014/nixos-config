2c6e575f8e0cbaf6fca2ad48ba562b4f879b9169
Moritz Vogel <moritzv7@gmail.com>
2025-01-27 19:26:38 +0100

    einkaufszettel: 2025-01-18 -> 2025-01-27

--- packages/einkaufszettel.nix
+++ packages/einkaufszettel.nix
@@ -2,13 +2,13 @@
 
 rustPlatform.buildRustPackage {
   name = "einkaufszettel";
-  version = "2025-01-18";
+  version = "2025-01-27";
 
   src = fetchFromGitHub {
     owner = "Birdy2014";
     repo = "einkaufszettel";
-    rev = "901c730675700b17b917a26eb15dca2b12e70dcf";
-    hash = "sha256-LFAwkHZGYFd25tSdnrhzNlN9ulmVsPQ92ezPl1Vh3kk=";
+    rev = "963ab400ac1c0c4de5bebf5a8a93ac568aa1eee9";
+    hash = "sha256-w1qmszuskB4nkKuLNTQ7KugfzIkf+IqDu8IJJH0QVlY=";
   };
 
   cargoHash = "sha256-8QPwt2FTXV6H692RFQjrJp3BHi8JxIEpFDDv3wR60ZY=";
