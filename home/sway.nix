5c6f990addc83317db669e52b8e523286b1472b1
Moritz Vogel <moritzv7@gmail.com>
2025-02-23 22:36:54 +0100

    sway: Ensure swayidle is enabled before hibernate

--- home/sway.nix
+++ home/sway.nix
@@ -113,7 +113,8 @@
           "exec systemctl --user --quiet start swayidle.service && pkill -SIGRTMIN+10 waybar && systemctl suspend";
         "Control+Mod4+Mod1+p" = "exec systemctl poweroff";
         "Control+Mod4+Mod1+r" = "exec systemctl reboot";
-        "Control+Mod4+Mod1+h" = "exec systemctl hibernate";
+        "Control+Mod4+Mod1+h" =
+          "exec systemctl --user --quiet start swayidle.service && pkill -SIGRTMIN+10 waybar && systemctl hibernate";
 
         "${modifier}+o" = "scratchpad show";
         "${modifier}+r" = "mode resize";
