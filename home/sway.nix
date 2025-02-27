187ed67b2b38923106e9ddfd867a9ac779433884
Moritz Vogel <moritzv7@gmail.com>
2025-02-28 00:00:47 +0100

    sway: Add keybind for neovide

--- home/sway.nix
+++ home/sway.nix
@@ -82,6 +82,7 @@
         "${modifier}+Control+w" = "exec firefox";
         "${modifier}+Control+y" = "exec firefox -p persistent";
         "${modifier}+Control+n" = "exec ${terminal} ${pkgs.lf}/bin/lf";
+        "${modifier}+Control+e" = "exec neovide";
         "Print" = "exec flameshot gui";
 
         # Layout
