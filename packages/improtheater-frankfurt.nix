0562d03fdbfe62b14022667e6e0a89c25a58bd0d
Moritz Vogel <moritzv7@gmail.com>
2025-04-16 19:09:37 +0200

    improtheater-frankfurt: Reduce closure size from 804.85 MiB to 263.89 MiB

--- packages/improtheater-frankfurt.nix
+++ packages/improtheater-frankfurt.nix
@@ -30,6 +30,17 @@ buildNpmPackage {
 
   dontNpmBuild = true;
 
+  # Reduce closure size
+  postInstall = ''
+    prefix="$out/lib/node_modules/improtheater-frankfurt/node_modules"
+    rm $prefix/better-sqlite3/build/Makefile
+    rm $prefix/better-sqlite3/build/config.gypi
+    rm $prefix/better-sqlite3/build/better_sqlite3.target.mk
+    rm $prefix/better-sqlite3/build/test_extension.target.mk
+    rm -r $prefix/better-sqlite3/build/deps
+    rm -r $prefix/better-sqlite3/build/Release/.deps
+  '';
+
   npmInstallFlags = [ "--build-from-source" ];
   makeCacheWritable = true;
 }
