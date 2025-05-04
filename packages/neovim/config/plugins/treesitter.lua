c4205692cb9ff59e1bf245c31edecc6ae42d8638
Moritz Vogel <moritzv7@gmail.com>
2025-05-05 01:17:32 +0200

    nvim: Change incremental selection keymaps
    This fixes selecting lines in `q:` and `q/`.

--- packages/neovim/config/plugins/treesitter.lua
+++ packages/neovim/config/plugins/treesitter.lua
@@ -9,9 +9,9 @@ require("nvim-treesitter.configs").setup {
     incremental_selection = {
         enable = true,
         keymaps = {
-            init_selection = "<CR>",
-            node_incremental = "<CR>",
-            node_decremental = "<BS>",
+            init_selection = "+",
+            node_incremental = "+",
+            node_decremental = "-",
         },
     }
 }
