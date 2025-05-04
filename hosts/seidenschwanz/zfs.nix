6c1347304df2509f34ffa9d577e285e24cbe38c7
Moritz Vogel <moritzv7@gmail.com>
2025-05-04 16:38:17 +0200

    seidenschwanz: Fix zed monitoring emails

--- hosts/seidenschwanz/zfs.nix
+++ hosts/seidenschwanz/zfs.nix
@@ -1,4 +1,4 @@
-{ lib, ... }:
+{ config, lib, pkgs, ... }:
 
 {
   boot.supportedFilesystems = [ "zfs" ];
@@ -56,10 +56,53 @@
     };
   };
 
-  services.zfs.zed.settings = {
-    ZED_EMAIL_ADDR = [ "moritzv7+monitoring@gmail.com" ];
-    ZED_EMAIL_PROG = "sendmail";
-    ZED_EMAIL_OPTS = "@ADDRESS@";
+  services.zfs.zed.settings = let
+    # The email subject has to be translated into a sendmail-style format
+    zedMail = pkgs.writers.writePython3 "zed-mail.py" {
+      flakeIgnore = [ "E111" "E501" "E121" ];
+    } # python
+      ''
+        import sys
+        import subprocess
+
+        message: list[str] = []
+        email_subject: str = ""
+        email_from = "${config.services.nullmailer.config.allmailfrom}"
+        email_to = "${config.services.nullmailer.config.adminaddr}"
+
+        for line in sys.stdin:
+          message.append(line)
+
+        is_subject = False
+        for arg in sys.argv[1:]:
+          if is_subject:
+            email_subject = arg
+            is_subject = False
+            continue
+
+          if arg == "-s":
+            is_subject = True
+            continue
+
+        envelope_head = [
+          f"From: {email_from}",
+          f"To: {email_to}",
+          f"Subject: {email_subject}",
+        ]
+
+        sendmail_message = "\r\n".join(envelope_head) + "\r\n\r\n" + "\r\n".join(message)
+
+        process = subprocess.Popen(["/run/wrappers/bin/sendmail", "-v", email_to], stdin=subprocess.PIPE, stdout=subprocess.PIPE, text=True)
+        out, err = process.communicate(input=sendmail_message)
+        code = process.returncode
+        print(out)
+        print(err, file=sys.stderr)
+        exit(code)
+      '';
+  in {
+    ZED_EMAIL_ADDR = [ config.services.nullmailer.config.adminaddr ];
+    ZED_EMAIL_PROG = "${zedMail}";
+    ZED_EMAIL_OPTS = "-s @SUBJECT@";
 
     ZED_NOTIFY_INTERVAL_SECS = 3600;
     ZED_NOTIFY_VERBOSE = true;
