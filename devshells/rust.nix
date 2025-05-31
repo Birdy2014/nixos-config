59d0256f9a69f157496edcf5c458d6641e17befc
Moritz Vogel <moritzv7@gmail.com>
2025-05-31 20:10:03 +0200

    Fix rust devshell

--- devshells/rust.nix
+++ devshells/rust.nix
@@ -1,9 +1,8 @@
 { pkgs }:
 
-(import ./cpp.nix { inherit pkgs; }).overrideAttrs (final: prev: {
+pkgs.mkShell {
   name = "rust";
-  packages = prev.packages
-    ++ (with pkgs; [ cargo rustc rustfmt clippy rust-analyzer ]);
+  packages = with pkgs; [ cargo rustc rustfmt clippy rust-analyzer ];
   RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
   RUST_BACKTRACE = 1;
-})
+}
