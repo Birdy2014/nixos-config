c268ac773a8f9c0f03c4ab0bbc130650475a73fe
Moritz Vogel <moritzv7@gmail.com>
2025-06-13 21:54:20 +0200

    buntspecht: Disable nginx access log

--- hosts/buntspecht/nginx.nix
+++ hosts/buntspecht/nginx.nix
@@ -2,21 +2,6 @@
 
 {
   services.nginx.commonHttpConfig = ''
-    map $remote_addr $remote_addr_anon {
-      ~(?P<ip>\d+\.\d+\.\d+)\.    $ip.0;
-      ~(?P<ip>[^:]+:[^:]+):       $ip::;
-      default                     0.0.0.0;
-    }
-
-    log_format   anonip '$remote_addr_anon - $remote_user [$time_local]  $status '
-      '"$request" $body_bytes_sent "$http_referer" '
-      '"$http_user_agent"';
-
-    access_log   /var/log/nginx/access.log  anonip;
+    access_log off;
   '';
-
-  services.logrotate.settings.nginx = {
-    frequency = "daily";
-    rotate = 6;
-  };
 }
