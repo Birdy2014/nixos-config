d408de27ae7690acd00ce7ff675f80b82b3812cf
Moritz Vogel <moritzv7@gmail.com>
2025-06-19 21:35:09 +0200

    dashboard: self-host icons and use svg

--- hosts/seidenschwanz/services/dashboard/default.nix
+++ hosts/seidenschwanz/services/dashboard/default.nix
@@ -7,9 +7,20 @@
     onlySSL = true;
     useACMEHost = "seidenschwanz.mvogel.dev";
     listenAddresses = [ "[fd00:90::10]" ];
-    locations."/".root = pkgs.runCommand "seidenschwanz-dashboard" { } ''
-      mkdir "$out"
-      cp ${./dashboard.html} "$out/index.html"
-    '';
+    locations."/".root =
+      let
+        dashboard-icons = pkgs.fetchFromGitHub {
+          owner = "homarr-labs";
+          repo = "dashboard-icons";
+          rev = "d508f3708c27c492879aeed981daece06ce451d4";
+          hash = "sha256-XviHDJIv+HhFHb18kNPYF1lLTk2IhxvvYayz0dZ4NWA=";
+          sparseCheckout = [ "svg" ];
+        };
+      in
+      pkgs.runCommand "seidenschwanz-dashboard" { } ''
+        mkdir "$out"
+        cp ${./dashboard.html} "$out/index.html"
+        cp -r ${dashboard-icons}/svg "$out"
+      '';
   };
 }
