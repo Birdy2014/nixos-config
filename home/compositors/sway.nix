b75c0a0648b35ab0356e7a805b07cd74885d08b9
Moritz Vogel <moritzv7@gmail.com>
2025-06-22 16:59:34 +0200

    sway: Use lib.range to remove some unnecessary lines

--- home/compositors/sway.nix
+++ home/compositors/sway.nix
@@ -261,22 +261,7 @@
                 )
               ));
           in
-          workspaceKeybinding [
-            1
-            2
-            3
-            4
-            5
-            6
-            7
-            8
-            9
-            10
-            11
-            12
-            13
-            14
-          ]
+          workspaceKeybinding (lib.range 1 14)
           + ''
             title_align center
             titlebar_border_thickness 0
