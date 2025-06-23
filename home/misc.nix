a72899ec624f552591cade398e750dad9a993659
Moritz Vogel <moritzv7@gmail.com>
2025-06-23 18:20:30 +0200

    Move fcitx5 to home-manager config
    This also fixes a warning regarding GTK_IM_MODULE being set.

--- home/misc.nix
+++ home/misc.nix
@@ -173,4 +173,36 @@
         unshareNet = false;
       };
     };
+
+  i18n.inputMethod = {
+    enable = true;
+    type = "fcitx5";
+    fcitx5 = {
+      waylandFrontend = true;
+      addons = with pkgs; [
+        fcitx5-mozc
+        fcitx5-gtk
+      ];
+      settings = {
+        inputMethod = {
+          "Groups/0" = {
+            Name = "Default";
+            "Default Layout" = "de-nodeadkeys";
+            DefaultIM = "mozc";
+          };
+          "Groups/0/Items/0" = {
+            Name = "keyboard-de-nodeadkeys";
+            Layout = "";
+          };
+          "Groups/0/Items/1" = {
+            Name = "mozc";
+            Layout = "";
+          };
+          "GroupOrder"."0" = "Default";
+        };
+        # The F13 key is called "Tools" for some reason
+        globalOptions."Hotkey/TriggerKeys"."0" = "Tools";
+      };
+    };
+  };
 }
