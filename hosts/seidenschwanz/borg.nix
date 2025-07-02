ea5d725933249272ee94986e3d312822661b919f
Moritz Vogel <moritzv7@gmail.com>
2025-07-02 14:10:41 +0200

    seidenschwanz: Backup postgresql database

--- hosts/seidenschwanz/borg.nix
+++ hosts/seidenschwanz/borg.nix
@@ -1,29 +1,48 @@
-{ ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 
 {
-  services.borgbackup.jobs.seidenschwanz-services = {
-    user = "root";
-    group = "root";
-    paths = [
-      "/etc/ssh"
-      "/var/lib/authelia-main"
-      "/var/lib/jellyfin"
-      "/var/lib/paperless"
-      "/var/lib/private/einkaufszettel"
-      "/var/lib/private/lldap"
-      "/var/lib/private/mealie"
-    ];
-    exclude = [ "/var/lib/jellyfin/transcodes" ];
-    compression = "zstd,10";
-    repo = "/zpool/backup/seidenschwanz-services";
-    encryption.mode = "none";
-    startAt = "*-*-* 00:00:00";
-    persistentTimer = true;
-    prune.keep = {
-      within = "1d"; # Keep all archives from the last day
-      daily = 7;
-      weekly = 4;
-      monthly = 6;
+  services.borgbackup.jobs.seidenschwanz-services =
+    let
+      # This directory must exist and be owned by postgres:postgres
+      dbBackupDir = "/var/backup/postgresqldump";
+      pg_dump = lib.getExe' config.services.postgresql.finalPackage "pg_dump";
+    in
+    {
+      user = "root";
+      group = "root";
+      paths = [
+        "/etc/ssh"
+        "/var/lib/authelia-main"
+        "/var/lib/jellyfin"
+        "/var/lib/paperless"
+        "/var/lib/private/einkaufszettel"
+        "/var/lib/private/lldap"
+        "/var/lib/private/mealie"
+        dbBackupDir
+      ];
+      exclude = [ "/var/lib/jellyfin/transcodes" ];
+      compression = "zstd,10";
+      repo = "/zpool/backup/seidenschwanz-services";
+      encryption.mode = "none";
+      startAt = "*-*-* 00:00:00";
+      persistentTimer = true;
+      prune.keep = {
+        within = "1d"; # Keep all archives from the last day
+        daily = 7;
+        weekly = 4;
+        monthly = 6;
+      };
+      readWritePaths = [ dbBackupDir ];
+      preHook =
+        lib.concatMapStringsSep "\n"
+          (dbName: "${lib.getExe pkgs.sudo} -u postgres ${pg_dump} -f ${dbBackupDir}/${dbName} ${dbName}")
+          [
+            "immich"
+          ];
     };
-  };
 }
