16d924fc1e9a12aabd4be7fb9178df439b97db09
Moritz Vogel <moritzv7@gmail.com>
2025-09-06 23:16:58 +0200

    neovim: Disable async treesitter parsing to reduce errors
    At least I hope that this will eliminate one annoying but difficult to
    reproduce error.

--- packages/neovim/config/plugins/treesitter.lua
+++ packages/neovim/config/plugins/treesitter.lua
@@ -18,3 +18,7 @@ require("nvim-treesitter.configs").setup({
 
 vim.wo.foldmethod = "expr"
 vim.wo.foldexpr = "nvim_treesitter#foldexpr()"
+
+-- This might fix the treesitter error when removing lines in some files
+-- TODO: Try disabling this on neovim 0.11.4 or 0.12 (NixOS 25.11)
+vim.g._ts_force_sync_parsing = true
