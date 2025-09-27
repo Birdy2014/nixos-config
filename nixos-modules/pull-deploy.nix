f6f499c07d2f31fbde107f8d4080705bc2646037
Moritz Vogel <moritzv7@gmail.com>
2025-09-27 13:43:29 +0200

    pull-deploy: Fix git pull in secrets repo

--- nixos-modules/pull-deploy.nix
+++ nixos-modules/pull-deploy.nix
@@ -8,6 +8,35 @@
 
 let
   cfg = config.my.pull-deploy;
+
+  hook = pkgs.writeShellApplication {
+    name = "hook.sh";
+    runtimeInputs = [
+      pkgs.git
+      pkgs.openssh
+    ]
+    ++ (lib.optional cfg.notify pkgs.dbus);
+    text = ''
+      if [[ "$DEPLOY_STATUS" == 'pre' ]] && [[ -d '/etc/nixos-secrets' ]]; then
+        git -C /etc/nixos-secrets pull
+      fi
+
+      ${lib.optionalString cfg.notify ''
+        send_notification() {
+          dbus-send --system / net.nuetzlich.SystemNotifications.Notify \
+            'string:nixos-pull-deploy' "string:$1"
+        }
+
+        if [[ "$DEPLOY_STATUS" == 'success' ]]; then
+          send_notification 'Aktualisierung erfolgreich abgeschlossen'
+        fi
+
+        if [[ "$DEPLOY_STATUS" == 'failed' ]]; then
+          send_notification 'Aktualisierung fehlgeschlagen'
+        fi
+      ''}
+    '';
+  };
 in
 {
   imports = [ inputs.nixos-pull-deploy.nixosModules.default ];
@@ -36,25 +65,7 @@ in
           main = "main";
           testing = "testing-";
         };
-        hook = pkgs.writeShellScript "hook.sh" ''
-          if [[ "$DEPLOY_STATUS" == 'pre' ]] && [[ -d '/etc/nixos-secrets' ]]; then
-            git -C /etc/nixos-secrets pull
-          fi
-
-          ${lib.optionalString cfg.notify ''
-            if [[ "$DEPLOY_STATUS" == 'success' ]]; then
-              ${lib.getExe' pkgs.dbus "dbus-send"} \
-                --system / net.nuetzlich.SystemNotifications.Notify \
-                'string:nixos-pull-deploy' 'string:Aktualisierung erfolgreich abgeschlossen'
-            fi
-
-            if [[ "$DEPLOY_STATUS" == 'failed' ]]; then
-              ${lib.getExe' pkgs.dbus "dbus-send"} \
-                --system / net.nuetzlich.SystemNotifications.Notify \
-                'string:nixos-pull-deploy' 'string:Aktualisierung fehlgeschlagen'
-            fi
-          ''}
-        '';
+        hook = "${hook}/bin/hook.sh";
       };
     };
   };
