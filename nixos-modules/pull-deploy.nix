c5df7bc7bea1f3894ade6d10b7f83250795d15b3
Moritz Vogel <moritzv7@gmail.com>
2025-10-13 12:16:50 +0200

    pull-deploy: Add randomized delay

--- nixos-modules/pull-deploy.nix
+++ nixos-modules/pull-deploy.nix
@@ -87,7 +87,10 @@ in
     systemd.timers.nixos-pull-deploy = {
       wants = [ "network-online.target" ];
       after = [ "network-online.target" ];
-      timerConfig.Persistent = true;
+      timerConfig = {
+        Persistent = true;
+        RandomizedDelaySec = "10min";
+      };
     };
   };
 }
