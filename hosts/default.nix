b32a30b2087c43311be026cfd79ff26fdd26e575
Moritz Vogel <moritzv7@gmail.com>
2025-10-24 22:04:45 +0200

    Get hosts from directory listing

--- hosts/default.nix
+++ hosts/default.nix
@@ -1,32 +1,22 @@
 { lib, inputs }:
 
-let
-  hosts = [
-    "rotkehlchen"
-    "zilpzalp"
-    "singdrossel"
-    "seidenschwanz"
-    "buntspecht"
-    "bergfink"
-  ];
-in
-(builtins.listToAttrs (
-  map (name: {
-    name = name;
-    value = lib.nixosSystem {
-      specialArgs = {
-        inherit inputs;
-        myLib = import ../lib {
-          inherit lib;
-          inherit (inputs) nix-colorizer;
-        };
+builtins.readDir ./.
+|> lib.filterAttrs (name: type: type == "directory")
+|> lib.mapAttrs (
+  name: _:
+  lib.nixosSystem {
+    specialArgs = {
+      inherit inputs;
+      myLib = import ../lib {
+        inherit lib;
+        inherit (inputs) nix-colorizer;
       };
-      modules = [
-        ./${name}
-        ../nixos-modules
-        ../secrets
-        { networking.hostName = name; }
-      ];
     };
-  }) hosts
-))
+    modules = [
+      ./${name}
+      ../nixos-modules
+      ../secrets
+      { networking.hostName = name; }
+    ];
+  }
+)
