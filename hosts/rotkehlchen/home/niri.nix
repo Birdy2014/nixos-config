c0f1fed381ce0205b72bb73937ecc52fe62237da
Moritz Vogel <moritzv7@gmail.com>
2025-11-03 20:32:31 +0100

    rotkehlchen: Add niri window rule for virt-manager

--- hosts/rotkehlchen/home/niri.nix
+++ hosts/rotkehlchen/home/niri.nix
@@ -2,10 +2,24 @@
 
 {
   config = lib.mkIf config.programs.niri.enable {
-    programs.niri.settings.spawn-at-startup = [
-      { command = [ "element-desktop" ]; }
-      { command = [ "vesktop" ]; }
-      { command = [ "thunderbird" ]; }
-    ];
+    programs.niri.settings = {
+      spawn-at-startup = [
+        { command = [ "element-desktop" ]; }
+        { command = [ "vesktop" ]; }
+        { command = [ "thunderbird" ]; }
+      ];
+
+      window-rules = [
+        {
+          matches = [
+            {
+              app-id = "^.virt-manager-wrapped$";
+              title = "^.+ on QEMU/KVM(: .+)?$";
+            }
+          ];
+          default-column-width.proportion = 0.5;
+        }
+      ];
+    };
   };
 }
