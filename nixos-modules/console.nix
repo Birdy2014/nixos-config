10473fbd05f3a3ebeb572561033351da4a3d41c0
Moritz Vogel <moritzv7@gmail.com>
2025-12-12 00:51:34 +0100

    Fix console font

--- nixos-modules/console.nix
+++ nixos-modules/console.nix
@@ -1,9 +1,10 @@
-{ lib, ... }:
+{ lib, pkgs, ... }:
 
 {
   console = {
-    font = "Lat2-Terminus16";
+    font = "${pkgs.terminus_font}/share/consolefonts/ter-116n.psf.gz";
     useXkbConfig = true;
+    earlySetup = true;
   };
 
   services.xserver.xkb = {
