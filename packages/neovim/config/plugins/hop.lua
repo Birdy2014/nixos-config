d4275ce8306ba4fcb9e08746ebf43fcf1e39ed85
Moritz Vogel <moritzv7@gmail.com>
2026-01-17 16:07:20 +0100

    nvim: Bind s to hint_char2 instead of hint_words

--- packages/neovim/config/plugins/hop.lua
+++ packages/neovim/config/plugins/hop.lua
@@ -2,11 +2,9 @@ local hop = require("hop")
 
 hop.setup()
 
-vim.keymap.set({ "n", "v" }, "s", hop.hint_words, {
+vim.keymap.set({ "n", "v" }, "s", hop.hint_char2, {
     desc = "Hop words",
 })
-vim.keymap.set({ "n", "v" }, "S", function()
-    hop.hint_words({ multi_windows = true })
-end, {
-    desc = "Hop word Multi Window",
+vim.keymap.set({ "n", "v" }, "S", hop.hint_words, {
+    desc = "Hop words",
 })
