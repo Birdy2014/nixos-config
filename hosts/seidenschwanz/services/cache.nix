bec0fc4599b6970e90f356c964001e92178c5338
Moritz Vogel <moritzv7@gmail.com>
2026-01-17 15:23:45 +0100

    seidenschwanz/cache: Switch from nix-serve-ng to harmonia

--- hosts/seidenschwanz/services/cache.nix
+++ hosts/seidenschwanz/services/cache.nix
@@ -1,20 +1,15 @@
-{ config, pkgs, ... }:
+{ config, ... }:
 
 {
-  services.nix-serve = {
+  services.harmonia = {
     enable = true;
-    package = pkgs.nix-serve-ng;
-    secretKeyFile = config.sops.secrets.cache-private-key.path;
-    bindAddress = "127.0.0.1";
+    signKeyPaths = [ config.sops.secrets.cache-private-key.path ];
+    settings = {
+      bind = "[::]:5000";
+      # compression defaults to false with next version?
+      # enable_compression = true;
+    };
   };
 
-  my.proxy.domains.cache.proxyPass =
-    "http://${config.services.nix-serve.bindAddress}:${toString config.services.nix-serve.port}";
-
-  services.nginx.virtualHosts."cache.seidenschwanz.mvogel.dev".locations."/".extraConfig = ''
-    gzip on;
-    gzip_static on;
-    gzip_vary on;
-    gzip_types text/plain;
-  '';
+  my.proxy.domains.cache.proxyPass = "http://${config.services.harmonia.settings.bind}";
 }
