2b6390e957b4401a831e50db98fdfb8acc7b5de4
Moritz Vogel <moritzv7@gmail.com>
2026-01-26 16:22:09 +0100

    rotkehlchen: Refactor filesystem mounts

--- hosts/rotkehlchen/btrbk.nix
+++ hosts/rotkehlchen/btrbk.nix
@@ -12,24 +12,16 @@
       };
       onCalendar = "hourly";
     };
-  }
-  // (builtins.listToAttrs (
-    map
-      (mountpoint: {
-        name = baseNameOf mountpoint;
-        value = {
-          settings = {
-            snapshot_preserve = "7d 8h";
-            snapshot_preserve_min = "2h";
-            snapshot_dir = "${mountpoint}/snapshots";
-            subvolume."${mountpoint}" = { };
-          };
-          onCalendar = "hourly";
-        };
-      })
-      [
-        "/run/media/moritz/archive"
-        "/run/media/moritz/games"
-      ]
-  ));
+
+    archive = {
+      settings = {
+        snapshot_preserve = "7d 8h";
+        snapshot_preserve_min = "2h";
+        snapshot_dir = "/run/media/moritz/snapshots";
+        subvolume."/run/media/moritz/archive" = { };
+        subvolume."/run/media/moritz/games" = { };
+      };
+      onCalendar = "hourly";
+    };
+  };
 }
--- hosts/rotkehlchen/filesystems.nix
+++ hosts/rotkehlchen/filesystems.nix
@@ -1,148 +1,80 @@
-{ ... }:
+{ lib, ... }:
 
 {
-  # Samsung SSD 980 PRO with Heatsink 2TB
+  boot.initrd.luks.devices =
+    let
+      devices = {
+        # Samsung SSD 980 PRO with Heatsink 2TB
+        "nixos-root" = "/dev/disk/by-uuid/2710437f-5af4-46b1-a837-0c046c3e3209";
 
-  boot.initrd.luks.devices."nixos-root" = {
-    device = "/dev/disk/by-uuid/2710437f-5af4-46b1-a837-0c046c3e3209";
-    bypassWorkqueues = true;
-    allowDiscards = true;
-  };
+        # Samsung SSD 870 EVO 4TB
+        "archive" = "/dev/disk/by-uuid/6198ff28-d300-4217-a480-a855ef0a8867";
 
-  fileSystems."/" = {
-    device = "/dev/disk/by-uuid/b6d597be-f401-45a6-a7aa-e9c5498745f0";
-    fsType = "btrfs";
-    options = [
-      "subvol=rootfs"
-      "compress=zstd:1"
-      "noatime"
-      "nosuid"
-    ];
-  };
+        # Samsung SSD 870 QVO 2TB
+        "archive2" = "/dev/disk/by-uuid/ff1db63b-e1b0-494e-8425-f20d23e9ffc1";
 
-  fileSystems."/home" = {
-    device = "/dev/disk/by-uuid/b6d597be-f401-45a6-a7aa-e9c5498745f0";
-    fsType = "btrfs";
-    options = [
-      "subvol=home"
-      "compress=zstd:1"
-      "noatime"
-      "nosuid"
-    ];
-    neededForBoot = true; # Needed for sops to find the key
-  };
+        # Crucial CT1000P1SSD8
+        "archive3" = "/dev/disk/by-uuid/b66a028b-9986-4c08-b674-bb2c5129620b";
+      };
+    in
+    lib.mapAttrs (name: device: {
+      inherit device;
+      bypassWorkqueues = true;
+      allowDiscards = true;
+    }) devices;
 
-  fileSystems."/nix" = {
-    device = "/dev/disk/by-uuid/b6d597be-f401-45a6-a7aa-e9c5498745f0";
-    fsType = "btrfs";
-    options = [
-      "subvol=nix"
-      "compress=zstd:1"
-      "noatime"
-      "nodev"
-      "nosuid"
-    ];
-  };
+  fileSystems =
+    let
+      mkOptionsBtrfs =
+        subvol: extra:
+        [
+          "subvol=${subvol}"
+          "compress=zstd:1"
+          "noatime"
+          "nodev"
+          "nosuid"
+        ]
+        ++ extra;
 
-  fileSystems."/swap" = {
-    device = "/dev/disk/by-uuid/b6d597be-f401-45a6-a7aa-e9c5498745f0";
-    fsType = "btrfs";
-    options = [
-      "subvol=swap"
-      "compress=zstd:1"
-      "noatime"
-      "nodev"
-      "nosuid"
-      "noexec"
-    ];
-  };
+      bootSubvolumes = {
+        "/" = mkOptionsBtrfs "rootfs" [ ];
+        "/home" = mkOptionsBtrfs "home" [ ];
+        "/nix" = mkOptionsBtrfs "nix" [ ];
+        "/swap" = mkOptionsBtrfs "swap" [ "noexec" ];
+        "/vm-images" = mkOptionsBtrfs "vm-images" [ "noexec" ];
+        "/snapshots" = mkOptionsBtrfs "snapshots" [ "noexec" ];
+      };
 
-  fileSystems."/vm-images" = {
-    device = "/dev/disk/by-uuid/b6d597be-f401-45a6-a7aa-e9c5498745f0";
-    fsType = "btrfs";
-    options = [
-      "subvol=vm-images"
-      "compress=zstd:1"
-      "noatime"
-      "nodev"
-      "nosuid"
-      "noexec"
-    ];
-  };
-
-  fileSystems."/snapshots" = {
-    device = "/dev/disk/by-uuid/b6d597be-f401-45a6-a7aa-e9c5498745f0";
-    fsType = "btrfs";
-    options = [
-      "subvol=snapshots"
-      "compress=zstd:1"
-      "noatime"
-      "nodev"
-      "nosuid"
-      "noexec"
-    ];
-  };
-
-  fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/C01C-AFCC";
-    fsType = "vfat";
-    options = [
-      "umask=0077"
-      "noatime"
-      "nodev"
-      "nosuid"
-      "noexec"
-    ];
-  };
-
-  # Crucial CT1000P1SSD8
-
-  boot.initrd.luks.devices."games" = {
-    device = "/dev/disk/by-uuid/b66a028b-9986-4c08-b674-bb2c5129620b";
-    bypassWorkqueues = true;
-    allowDiscards = true;
-  };
-
-  fileSystems."/run/media/moritz/games" = {
-    device = "/dev/disk/by-uuid/8b85ed66-baa2-42b4-b179-c0f6aaa2a555";
-    fsType = "btrfs";
-    options = [
-      "compress=zstd:1"
-      "noatime"
-      "nodev"
-      "nosuid"
-    ];
-  };
-
-  # Samsung SSD 870 EVO 4TB
-
-  boot.initrd.luks.devices."archive" = {
-    device = "/dev/disk/by-uuid/6198ff28-d300-4217-a480-a855ef0a8867";
-    bypassWorkqueues = true;
-    allowDiscards = true;
-  };
-
-  fileSystems."/run/media/moritz/archive" = {
-    device = "/dev/disk/by-uuid/985dc3e3-86dc-48e2-9bd5-ee7b3ab798af";
-    fsType = "btrfs";
-    options = [
-      "compress=zstd:3"
-      "noatime"
-      "nodev"
-      "nosuid"
-      "noexec"
-    ];
-  };
-
-  # Samsung SSD 870 QVO 2TB
-
-  boot.initrd.luks.devices."archive2" = {
-    device = "/dev/disk/by-uuid/ff1db63b-e1b0-494e-8425-f20d23e9ffc1";
-    bypassWorkqueues = true;
-    allowDiscards = true;
-  };
-
-  # "archive2" is a part of the "archive" btrfs volume
+      archiveSubvolumes = {
+        "/run/media/moritz/archive" = mkOptionsBtrfs "archive" [ "noexec" ];
+        "/run/media/moritz/games" = mkOptionsBtrfs "games" [ ];
+        "/run/media/moritz/snapshots" = mkOptionsBtrfs "snapshots" [ "noexec" ];
+      };
+    in
+    (lib.mapAttrs (name: options: {
+      device = "/dev/disk/by-uuid/b6d597be-f401-45a6-a7aa-e9c5498745f0";
+      fsType = "btrfs";
+      inherit options;
+      neededForBoot = true; # Needed on /home for sops to find the key
+    }) bootSubvolumes)
+    // {
+      "/boot" = {
+        device = "/dev/disk/by-uuid/C01C-AFCC";
+        fsType = "vfat";
+        options = [
+          "umask=0077"
+          "noatime"
+          "nodev"
+          "nosuid"
+          "noexec"
+        ];
+      };
+    }
+    // (lib.mapAttrs (name: options: {
+      device = "/dev/disk/by-uuid/985dc3e3-86dc-48e2-9bd5-ee7b3ab798af";
+      fsType = "btrfs";
+      inherit options;
+    }) archiveSubvolumes);
 
   swapDevices = [ { device = "/swap/swapfile"; } ];
 
