900857229c76c4231588ace492bb7ca7bfe65ad1
Moritz Vogel <moritzv7@gmail.com>
2026-03-13 18:50:14 +0100

    seidenschwanz/acme: Use credentialFiles instead of sops template

--- hosts/seidenschwanz/services/acme.nix
+++ hosts/seidenschwanz/services/acme.nix
@@ -1,17 +1,16 @@
-{ config, ... }:
+{ config, pkgs, ... }:
 
 {
   security.acme.certs."seidenschwanz.mvogel.dev" = {
     extraDomainNames = [ "*.seidenschwanz.mvogel.dev" ];
     dnsProvider = "rfc2136";
     dnsResolver = "1.1.1.1:53";
-    environmentFile = config.sops.templates."bind-acme-certs".path;
+    environmentFile = pkgs.writeText "lego-seidenschwanz.mvogel.dev" ''
+      RFC2136_NAMESERVER=ns1.mvogel.dev
+      RFC2136_TSIG_ALGORITHM=hmac-sha256.
+      RFC2136_TSIG_KEY=seidenschwanz.mvogel.dev
+    '';
+    credentialFiles."RFC2136_TSIG_SECRET_FILE" =
+      config.sops.secrets."bind-dnskey_seidenschwanz.mvogel.dev".path;
   };
-
-  sops.templates."bind-acme-certs".content = ''
-    RFC2136_NAMESERVER=ns1.mvogel.dev
-    RFC2136_TSIG_ALGORITHM=hmac-sha256.
-    RFC2136_TSIG_KEY=seidenschwanz.mvogel.dev
-    RFC2136_TSIG_SECRET=${config.sops.placeholder."bind-dnskey_seidenschwanz.mvogel.dev"}
-  '';
 }
