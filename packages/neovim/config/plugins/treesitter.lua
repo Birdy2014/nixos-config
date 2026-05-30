352d9c80ec67c445b9e2b009bafb69f9bb9bce39
Moritz Vogel <moritzv7@gmail.com>
2026-05-30 20:36:36 +0200

    nvim: Fix nvim-treesitter usage

--- packages/neovim/config/plugins/treesitter.lua
+++ packages/neovim/config/plugins/treesitter.lua
@@ -1,24 +1,4 @@
-require("nvim-treesitter.configs").setup({
-    highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = false,
-    },
-    indent = {
-        enable = true,
-    },
-    incremental_selection = {
-        enable = true,
-        keymaps = {
-            init_selection = "+",
-            node_incremental = "+",
-            node_decremental = "-",
-        },
-    },
-})
-
 vim.wo.foldmethod = "expr"
 vim.wo.foldexpr = "nvim_treesitter#foldexpr()"
 
--- This might fix the treesitter error when removing lines in some files
--- TODO: Try disabling this on neovim 0.11.4 or 0.12 (NixOS 25.11)
-vim.g._ts_force_sync_parsing = true
+vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
