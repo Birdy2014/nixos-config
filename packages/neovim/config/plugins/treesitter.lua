b9a015ed2d6d9ca7863d451761ab9b08fe0eac9e
Moritz Vogel <moritzv7@gmail.com>
2026-06-15 23:36:47 +0200

    nvim: Fix starting of treesitter parsers

--- packages/neovim/config/plugins/treesitter.lua
+++ packages/neovim/config/plugins/treesitter.lua
@@ -1,4 +1,9 @@
-vim.wo.foldmethod = "expr"
-vim.wo.foldexpr = "nvim_treesitter#foldexpr()"
-
-vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+vim.api.nvim_create_autocmd("FileType", {
+    callback = function(args)
+        if pcall(vim.treesitter.start, args.buf) then
+            vim.wo.foldmethod = "expr"
+            vim.wo.foldexpr = "nvim_treesitter#foldexpr()"
+            vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+        end
+    end,
+})
