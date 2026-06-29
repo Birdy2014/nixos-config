73d6e02c6f07e2df2ed930406828e7926fe1361d
Moritz Vogel <moritzv7@gmail.com>
2026-06-29 18:27:09 +0200

    Update einkaufszettel

--- packages/einkaufszettel.nix
+++ packages/einkaufszettel.nix
@@ -2,13 +2,13 @@
 
 rustPlatform.buildRustPackage {
   name = "einkaufszettel";
-  version = "2026-03-22";
+  version = "2026-06-29";
 
   src = fetchFromGitHub {
     owner = "Birdy2014";
     repo = "einkaufszettel";
-    rev = "9b31c73b81c6a035d8ecf6b2405c79ff654ac1ac";
-    hash = "sha256-ksr3jnZgyahahQSArhgasjlv91cqktDlLSAVsDYkuu4=";
+    rev = "b37e1948c47381aeef7eb063cc535ffa8f07db56";
+    hash = "sha256-eyCh4PamH3CW2YS4bYz/ndvNk2tznfaiiPkWePrQmMw=";
   };
 
   cargoHash = "sha256-N04731fDqcSrAQ2Y0U8YBXRgfvIMwyQXctfZ7MiYjho=";
