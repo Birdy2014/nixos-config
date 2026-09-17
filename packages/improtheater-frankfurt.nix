ae4fd0bd554cacca0d8e42d44b9793d624dd9bf6
Moritz Vogel <moritzv7@gmail.com>
2026-09-18 00:24:22 +0200

    Update improtheater-frankfurt

--- packages/improtheater-frankfurt.nix
+++ packages/improtheater-frankfurt.nix
@@ -7,13 +7,13 @@
 
 buildNpmPackage {
   pname = "improtheater-frankfurt";
-  version = "2026-09-10";
+  version = "2026-09-18";
 
   src = fetchFromGitHub {
     owner = "Birdy2014";
     repo = "improtheater-frankfurt.de";
-    rev = "31a89ff34680b898af02bcc5288ff92ff71fdb50";
-    hash = "sha256-kaSW8/alTk6fHkyU+kaXGbH1QqCPFKEHb58aBlV/kBA=";
+    rev = "9951df65fd9dc0c6e388612ff53bef8743863fac";
+    hash = "sha256-pigO/hnKuRUS5eZnCce40Mcwv0kwZxy+Dp9nGNiQIGA=";
   };
 
   npmDepsHash = "sha256-SNMuZhYmacT8yY4fZe5bQ+avVGS1iW816AlNWg+x4FQ=";
