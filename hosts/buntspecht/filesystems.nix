b5d4354da90ad7d46a2a2402c320971aa5fcae1e
Moritz Vogel <moritzv7@gmail.com>
2026-09-21 02:19:18 +0200

    buntspecht: Re-partition using btrfs

--- hosts/buntspecht/filesystems.nix
+++ hosts/buntspecht/filesystems.nix
@@ -2,16 +2,43 @@
 
 {
   fileSystems."/" = {
-    device = "/dev/sda1";
-    fsType = "ext4";
+    device = "/dev/disk/by-uuid/a28e54dc-8fe6-4cd0-9898-5971ea8f68a0";
+    fsType = "btrfs";
     options = [
+      "subvol=root"
+      "compress=zstd:3"
       "noatime"
       "nosuid"
     ];
   };
 
+  fileSystems."/nix" = {
+    device = "/dev/disk/by-uuid/a28e54dc-8fe6-4cd0-9898-5971ea8f68a0";
+    fsType = "btrfs";
+    options = [
+      "subvol=nix"
+      "compress=zstd:3"
+      "noatime"
+      "nodev"
+      "nosuid"
+    ];
+  };
+
+  fileSystems."/var/log" = {
+    device = "/dev/disk/by-uuid/a28e54dc-8fe6-4cd0-9898-5971ea8f68a0";
+    fsType = "btrfs";
+    options = [
+      "subvol=log"
+      "compress=zstd:3"
+      "noatime"
+      "nodev"
+      "nosuid"
+      "noexec"
+    ];
+  };
+
   fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/01D2-08DB";
+    device = "/dev/disk/by-uuid/6F50-C8A2";
     fsType = "vfat";
     options = [
       "umask=0077"
@@ -22,5 +49,5 @@
     ];
   };
 
-  swapDevices = [ { device = "/swap/swapfile"; } ];
+  swapDevices = [ { device = "/dev/disk/by-uuid/ed5eceab-bd11-4cd4-bc3a-43a6583692b9"; } ];
 }
